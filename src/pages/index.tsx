01a0c7e4a649f52e1cf2a64e7a4c20823906a4a8
s1f102103178 <s1f102103178@iniad.org>
2023-05-25 20:14:03 +0900

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,12 +25,62 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
+  const board:Number[][]=[
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+  ]
+
+  const underboard:Number[][]=[
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+  ]
+
+  if(isPlaying
   const existBomb = 0;
   const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
   const isFailure = userInputs.some((row, y) =>
     row.some((input, x) => input === 1 && bombMap[y][x] === 1)
   );
-  const board: number[][] = [];
+
+  const [cellStatus,setCellStatus]=useState([
+    if(existBomb===0){
+
+    }
+
+  ])
+
+let zeroList: { x: number; y: number }[]
+for () {
+  zeroList = // board + directions + userInputs + bombMap
+}
+let openedCount: number
+for () {
+  openedCount = // board
+}
+const isSuccess = // openedCount + bombCount
+let isFailure: boolean
+for () {
+  isFialure = // userInputs + bombMap
+}
+let isStarted: boolean
+for () {
+  isStarted = // userInputs
+}
 
   return (
     <div className={styles.App}>
